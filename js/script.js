510e13f727922d3f9c1067565743a1b8626a5873
Abdyl-Aziz <azizshihabov@mail.ru>
2022-12-20 17:00:49 +0300

    feat: add drag-over

--- js/script.js
+++ js/script.js
@@ -1,22 +1,57 @@
 window.addEventListener('load', () => {
 	const containerEl = document.querySelector('[data-drag-container]');
-	const containerItemEls = document.querySelectorAll('[data-drag-item]');
 	const containerItemLists = document.querySelectorAll('.container__item-list');
+	let containerItemEls;
 
-	let backlogArr = [],
-		progressArr = [],
-		completeArr = [],
-		holdArr = [];
+	let backlogArr = ['Release the course', 'Sit back and relax'],
+		progressArr = ['Work on projects', 'Listen to music'],
+		completeArr = ['Being cool', 'Getting stuff done'],
+		holdArr = ['Being uncool'];
 
 	let currentItem;
 
-	containerItemEls.forEach((item) => {
-		item.addEventListener('dragstart', (e) => {
-			const el = e.target;
-			el.classList.add('selected');
-			currentItem = el;
+	loadKanbanInfo();
+
+	function loadKanbanInfo() {
+		containerItemLists.forEach((item, idx) => {
+			if (idx === 0) {
+				backlogArr.forEach((arrItem) => {
+					createContainerItem(item, arrItem);
+				});
+			} else if (idx === 1) {
+				progressArr.forEach((arrItem) => {
+					createContainerItem(item, arrItem);
+				});
+			} else if (idx === 2) {
+				completeArr.forEach((arrItem) => {
+					createContainerItem(item, arrItem);
+				});
+			} else if (idx === 3) {
+				holdArr.forEach((arrItem) => {
+					createContainerItem(item, arrItem);
+				});
+			}
 		});
-	});
+
+		containerItemEls = document.querySelectorAll('[data-drag-item]');
+
+		containerItemEls.forEach((item) => {
+			item.addEventListener('dragstart', (e) => {
+				const el = e.target;
+				el.classList.add('selected');
+				currentItem = el;
+			});
+		});
+	}
+
+	function createContainerItem(item, arrItem) {
+		const li = document.createElement('li');
+		li.classList.add('container__item-li');
+		li.textContent = arrItem;
+		li.setAttribute('data-drag-item', '');
+		li.setAttribute('draggable', 'true');
+		item.appendChild(li);
+	}
 
 	containerItemLists.forEach((item) => {
 		item.addEventListener('drop', (e) => {
@@ -31,10 +66,27 @@ window.addEventListener('load', () => {
 			}
 
 			containerItemEls.forEach((item) => item.classList.remove('selected'));
+			containerItemLists.forEach((item) => item.classList.remove('drag-over'));
+		});
+
+		item.addEventListener('dragenter', (e) => {
+			const el = e.target;
+			addDragOver(el);
 		});
 
 		item.addEventListener('dragover', (e) => {
 			e.preventDefault();
+			const el = e.target;
+			containerItemLists.forEach((item) => item.classList.remove('drag-over'));
+			addDragOver(el);
 		});
 	});
+
+	function addDragOver(el) {
+		if (el.classList.contains('container__item-li')) {
+			el.parentNode.classList.add('drag-over');
+		} else {
+			el.classList.add('drag-over');
+		}
+	}
 });
